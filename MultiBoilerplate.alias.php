ab1bbb4ea56a543ac4cbf465386d1ddb7d1813d6
Umherirrender <umherirrender_de.wp@web.de>
2018-10-12 22:40:04 +0200

    Use short array syntax in alias file
    Change-Id: I33612eb28cb011df6857f3009b8659d2629e95cd

--- MultiBoilerplate.alias.php
+++ MultiBoilerplate.alias.php
@@ -5,56 +5,55 @@
  * @file
  * @ingroup Extensions
  */
-// @codingStandardsIgnoreFile
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'Boilerplates' => array( 'Boilerplates' ),
-);
+$specialPageAliases['en'] = [
+	'Boilerplates' => [ 'Boilerplates' ],
+];
 
-$specialPageAliases['qqq'] = array(
-	'Boilerplates' => array( 'Name of special page' ),
-);
+$specialPageAliases['qqq'] = [
+	'Boilerplates' => [ 'Name of special page' ],
+];
 
 /** Arabic (العربية)
  * @author Alnokta
  * @author Meno25
  */
-$specialPageAliases['ar'] = array(
-	'Boilerplates' => array( 'القوالب الجاهزة المتوفرة' ),
-);
+$specialPageAliases['ar'] = [
+	'Boilerplates' => [ 'القوالب الجاهزة المتوفرة' ],
+];
 
 /** Belarusian (Taraškievica orthography) (беларуская (тарашкевіца)‎)
  * @author EugeneZelenko
  * @author Jim-by
  */
-$specialPageAliases['be-tarask'] = array(
-	'Boilerplates' => array( 'Даступныя шаблёны старонак' ),
-);
+$specialPageAliases['be-tarask'] = [
+	'Boilerplates' => [ 'Даступныя шаблёны старонак' ],
+];
 
 /** Bengali (বাংলা)
  * @author Wikitanvir
  */
-$specialPageAliases['bn'] = array(
-	'Boilerplates' => array( 'সহজলভ্য বয়লারপ্লেটসমূহ' ),
-);
+$specialPageAliases['bn'] = [
+	'Boilerplates' => [ 'সহজলভ্য বয়লারপ্লেটসমূহ' ],
+];
 
 /** Breton (brezhoneg)
  * @author Fohanno
  * @author Fulup
  */
-$specialPageAliases['br'] = array(
-	'Boilerplates' => array( 'Patromoù hegerz' ),
-);
+$specialPageAliases['br'] = [
+	'Boilerplates' => [ 'Patromoù hegerz' ],
+];
 
 /** Bosnian (bosanski)
  * @author CERminator
  */
-$specialPageAliases['bs'] = array(
-	'Boilerplates' => array( 'Dostupni šabloni' ),
-);
+$specialPageAliases['bs'] = [
+	'Boilerplates' => [ 'Dostupni šabloni' ],
+];
 
 /** German (Deutsch)
  * @author Imre
@@ -62,233 +61,231 @@ $specialPageAliases['bs'] = array(
  * @author Revolus
  * @author Umherirrender
  */
-$specialPageAliases['de'] = array(
-	'Boilerplates' => array( 'Verfügbare Textvorlagen' ),
-);
+$specialPageAliases['de'] = [
+	'Boilerplates' => [ 'Verfügbare Textvorlagen' ],
+];
 
 /** Lower Sorbian (dolnoserbski)
  * @author Michawiki
  */
-$specialPageAliases['dsb'] = array(
-	'Boilerplates' => array( 'K dispoziciji stojece tekstowe elementy' ),
-);
+$specialPageAliases['dsb'] = [
+	'Boilerplates' => [ 'K dispoziciji stojece tekstowe elementy' ],
+];
 
 /** Spanish (español)
  * @author Crazymadlover
  * @author Imre
  * @author Translationista
  */
-$specialPageAliases['es'] = array(
-	'Boilerplates' => array( 'Plantillas disponibles' ),
-);
+$specialPageAliases['es'] = [
+	'Boilerplates' => [ 'Plantillas disponibles' ],
+];
 
 /** Finnish (suomi)
  * @author Cimon Avaro
  * @author Crt
  * @author Str4nd
  */
-$specialPageAliases['fi'] = array(
-	'Boilerplates' => array( 'Käytettävissä olevat vakiotekstit' ),
-);
+$specialPageAliases['fi'] = [
+	'Boilerplates' => [ 'Käytettävissä olevat vakiotekstit' ],
+];
 
 /** French (français)
  * @author Crochet.david
  * @author Grondin
  * @author IAlex
  */
-$specialPageAliases['fr'] = array(
-	'Boilerplates' => array( 'Modèles disponibles' ),
-);
+$specialPageAliases['fr'] = [
+	'Boilerplates' => [ 'Modèles disponibles' ],
+];
 
 /** Franco-Provençal (arpetan)
  * @author ChrisPtDe
  */
-$specialPageAliases['frp'] = array(
-	'Boilerplates' => array( 'Modèlos disponiblos' ),
-);
+$specialPageAliases['frp'] = [
+	'Boilerplates' => [ 'Modèlos disponiblos' ],
+];
 
 /** Galician (galego)
  * @author Toliño
  */
-$specialPageAliases['gl'] = array(
-	'Boilerplates' => array( 'Boilerplates dispoñibles' ),
-);
+$specialPageAliases['gl'] = [
+	'Boilerplates' => [ 'Boilerplates dispoñibles' ],
+];
 
 /** Swiss German (Alemannisch)
  * @author Als-Holder
  */
-$specialPageAliases['gsw'] = array(
-	'Boilerplates' => array( 'Verfiegbari Textböustei' ),
-);
-
+$specialPageAliases['gsw'] = [
+	'Boilerplates' => [ 'Verfiegbari Textböustei' ],
+];
 
 /** Hebrew (עברית)
  * @author Amire80
  * @author YaronSh
  */
-$specialPageAliases['he'] = array(
-	'Boilerplates' => array( 'Boilerplates', 'טקסטים קבועים מראש' ),
-);
+$specialPageAliases['he'] = [
+	'Boilerplates' => [ 'Boilerplates', 'טקסטים קבועים מראש' ],
+];
 
 /** Upper Sorbian (hornjoserbsce)
  * @author Michawiki
  */
-$specialPageAliases['hsb'] = array(
-	'Boilerplates' => array( 'K dispoziciji stejace tekstowe elementy' ),
-);
+$specialPageAliases['hsb'] = [
+	'Boilerplates' => [ 'K dispoziciji stejace tekstowe elementy' ],
+];
 
 /** Hungarian (magyar)
  * @author Glanthor Reviol
  */
-$specialPageAliases['hu'] = array(
-	'Boilerplates' => array( 'Elérhető sablonszövegek' ),
-);
+$specialPageAliases['hu'] = [
+	'Boilerplates' => [ 'Elérhető sablonszövegek' ],
+];
 
 /** Interlingua (interlingua)
  * @author McDutchie
  */
-$specialPageAliases['ia'] = array(
-	'Boilerplates' => array( 'Modellos disponibile' ),
-);
+$specialPageAliases['ia'] = [
+	'Boilerplates' => [ 'Modellos disponibile' ],
+];
 
 /** Indonesian (Bahasa Indonesia)
  * @author Bennylin
  */
-$specialPageAliases['id'] = array(
-	'Boilerplates' => array( '\'\'Boilerplates\'\' yang tersedia' ),
-);
+$specialPageAliases['id'] = [
+	'Boilerplates' => [ '\'\'Boilerplates\'\' yang tersedia' ],
+];
 
 /** Italian (italiano)
  * @author Beta16
  */
-$specialPageAliases['it'] = array(
-	'Boilerplates' => array( 'Testi standard disponibili' ),
-);
+$specialPageAliases['it'] = [
+	'Boilerplates' => [ 'Testi standard disponibili' ],
+];
 
 /** Japanese (日本語)
  * @author Fryed-peach
  * @author Hosiryuhosi
  * @author Shirayuki
  */
-$specialPageAliases['ja'] = array(
-	'Boilerplates' => array( '利用可能な定型句' ),
-);
-
+$specialPageAliases['ja'] = [
+	'Boilerplates' => [ '利用可能な定型句' ],
+];
 
 /** Colognian (Ripoarisch)
  * @author Purodha
  */
-$specialPageAliases['ksh'] = array(
-	'Boilerplates' => array( 'Müjjelesche Vörlare' ),
-);
+$specialPageAliases['ksh'] = [
+	'Boilerplates' => [ 'Müjjelesche Vörlare' ],
+];
 
 /** Luxembourgish (Lëtzebuergesch)
  * @author Robby
  */
-$specialPageAliases['lb'] = array(
-	'Boilerplates' => array( 'Disponibel Textschablounen' ),
-);
+$specialPageAliases['lb'] = [
+	'Boilerplates' => [ 'Disponibel Textschablounen' ],
+];
 
 /** Macedonian (македонски)
  * @author Bjankuloski06
  */
-$specialPageAliases['mk'] = array(
-	'Boilerplates' => array( 'Достапни подготвени текстови' ),
-);
+$specialPageAliases['mk'] = [
+	'Boilerplates' => [ 'Достапни подготвени текстови' ],
+];
 
 /** Norwegian Bokmål (norsk (bokmål)‎)
  * @author Nghtwlkr
  */
-$specialPageAliases['nb'] = array(
-	'Boilerplates' => array( 'Tilgjengelige standardtekster' ),
-);
+$specialPageAliases['nb'] = [
+	'Boilerplates' => [ 'Tilgjengelige standardtekster' ],
+];
 
 /** Dutch (Nederlands)
  * @author SPQRobin
  * @author Siebrand
  */
-$specialPageAliases['nl'] = array(
-	'Boilerplates' => array( 'Beschikbare sjablonen' ),
-);
+$specialPageAliases['nl'] = [
+	'Boilerplates' => [ 'Beschikbare sjablonen' ],
+];
 
 /** Occitan (occitan)
  * @author Cedric31
  */
-$specialPageAliases['oc'] = array(
-	'Boilerplates' => array( 'Modèls disponibles' ),
-);
+$specialPageAliases['oc'] = [
+	'Boilerplates' => [ 'Modèls disponibles' ],
+];
 
 /** Polish (polski)
  * @author Maikking
  * @author Sp5uhe
  */
-$specialPageAliases['pl'] = array(
-	'Boilerplates' => array( 'Dostępne wcześniej zdefiniowane treści' ),
-);
+$specialPageAliases['pl'] = [
+	'Boilerplates' => [ 'Dostępne wcześniej zdefiniowane treści' ],
+];
 
 /** Piedmontese (Piemontèis)
  * @author Dragonòt
  */
-$specialPageAliases['pms'] = array(
-	'Boilerplates' => array( '"Boilerplate" disponìbij' ),
-);
+$specialPageAliases['pms'] = [
+	'Boilerplates' => [ '"Boilerplate" disponìbij' ],
+];
 
 /** Portuguese (português)
  * @author Hamilton Abreu
  * @author Malafaya
  * @author Waldir
  */
-$specialPageAliases['pt'] = array(
-	'Boilerplates' => array( 'Textos disponíveis' ),
-);
+$specialPageAliases['pt'] = [
+	'Boilerplates' => [ 'Textos disponíveis' ],
+];
 
 /** Brazilian Portuguese (português do Brasil)
  * @author Eduardo.mps
  */
-$specialPageAliases['pt-br'] = array(
-	'Boilerplates' => array( 'Modelos disponíveis' ),
-);
+$specialPageAliases['pt-br'] = [
+	'Boilerplates' => [ 'Modelos disponíveis' ],
+];
 
 /** tarandíne (tarandíne)
  * @author Joetaras
  */
-$specialPageAliases['roa-tara'] = array(
-	'Boilerplates' => array( 'Pàggene da \'ngorporà disponibbele' ),
-);
+$specialPageAliases['roa-tara'] = [
+	'Boilerplates' => [ 'Pàggene da \'ngorporà disponibbele' ],
+];
 
 /** Russian (русский)
  * @author Kaganer
  * @author Александр Сигачёв
  */
-$specialPageAliases['ru'] = array(
-	'Boilerplates' => array( 'Доступные заготовки страниц' ),
-);
+$specialPageAliases['ru'] = [
+	'Boilerplates' => [ 'Доступные заготовки страниц' ],
+];
 
 /** Slovak (slovenčina)
  * @author Helix84
  */
-$specialPageAliases['sk'] = array(
-	'Boilerplates' => array( 'Dostupné šablóny' ),
-);
+$specialPageAliases['sk'] = [
+	'Boilerplates' => [ 'Dostupné šablóny' ],
+];
 
 /** Tagalog (Tagalog)
  * @author AnakngAraw
  */
-$specialPageAliases['tl'] = array(
-	'Boilerplates' => array( 'Makukuhang mga platong pampakulo' ),
-);
+$specialPageAliases['tl'] = [
+	'Boilerplates' => [ 'Makukuhang mga platong pampakulo' ],
+];
 
 /** Simplified Chinese (中文（简体）‎)
  * @author PhiLiP
  * @author Zoglun
  */
-$specialPageAliases['zh-hans'] = array(
-	'Boilerplates' => array( '预加载格式模板（boilerplates）' ),
-);
+$specialPageAliases['zh-hans'] = [
+	'Boilerplates' => [ '预加载格式模板（boilerplates）' ],
+];
 
 /** Traditional Chinese (中文（繁體）‎)
  * @author Wrightbus
  */
-$specialPageAliases['zh-hant'] = array(
-	'Boilerplates' => array( '預加載格式模板（boilerplates）' ),
-);
+$specialPageAliases['zh-hant'] = [
+	'Boilerplates' => [ '預加載格式模板（boilerplates）' ],
+];
